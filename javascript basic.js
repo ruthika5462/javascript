b2f3e853c287bc887783f6b9bd160cdb51c72d14
Ruthika <114504421+ruthika5462@users.noreply.github.com>
2022-10-20 13:12:27 +0530

    Add files via upload

---
+++ javascript basic.js
@@ -0,0 +1,72 @@
+let num = 4
+console.log(num)
+num = 9
+console.log(num)
+let  username = " ruthika's account "; 
+console.log(username)
+
+let radius = 5
+const pi =  3.14
+let area
+radius = 6
+area = pi*radius*radius
+console.log(area)
+
+let data = 8
+let user="ruthika";
+let num1= 256743289;
+console.log(num1*25);
+let num2 = 7
+console.log(typeof num2);
+let num3 =0xf
+console.log(num1);
+let num4 = 10000000
+console.log(num4)
+let num5=5/0
+console.log(num5);
+let num6 = -5/10;
+console.log(Number.Max_value *10)
+let  num7 = -6/0;
+console.log(Number.minvalue *10)
+let num8 =146890654268907524578n
+console.log(num8+2n);
+let msg = "ruthika"
+console.log(user)
+let firstname="ruthika"
+let lastname="naidu"
+let user1 = firstname + lastname
+console.log(user1)
+let user4 = 'ruthika  " reckon"'
+console.log(user4)
+let user5 = "na\tvin \vreddy"
+console.log(user5)
+let bool = 5 < 6
+console.log(typeof bool);
+let user9 = null
+console.log(typeof user);
+let user10
+console.log(typeof user10)
+let num9 =5
+console.log(typeof (5/"ruthika"))
+let num12 =6
+console.log(num,typeof num);
+let num14 =14
+console.log(num,typeof num);
+let num15 = Number("123")
+console.log(num,typeof num);
+let x
+console.log(x,typeof x);
+x=8
+console.log(x,typeof x)
+x= x + ""
+console.log(x,typeof x);
+x= x-2
+console.log(x,typeof x);
+x = !x
+console.log(x,typeof x);
+
+let x1 = parseInt("123 Ruthika")
+console.log(x)
+
+
+
